46fadb3f16c76062956edfc7a3005e990ac8f54a
unleashed <kkipkurui123@gmail.com>
2023-06-15 09:36:09 +0300

    add home, quote, calculator, notmatch routes

---
+++ src/routes/Quote.jsx
@@ -0,0 +1,12 @@
+import Quotes from '../components/quote';
+
+const Quote = () => (
+  <div>
+    {/* other JSX code */}
+    <h1>Math Magicians</h1>
+    <Quotes />
+    {/* other JSX code */}
+  </div>
+);
+
+export default Quote;
